ab9fc7917635f2316e8f4be7b8b15128ff521b36
Gabriel Nicolau <39524325+7Nic@users.noreply.github.com>
2018-10-18 22:27:28 -0300

    Update main.c

--- main.c
+++ main.c
@@ -46,30 +46,5 @@ while(loop == TRUE){
     }
     pressEnter();
 }
-// InsertLoop(&root, 5, 5);
-// InsertLoop(&root, 3, 3);
-// InsertLoop(&root, 7, 7);
-// InsertLoop(&root, 2, 2);
-// InsertLoop(&root, 4, 4);
-// InsertLoop(&root, 6, 6);
-// InsertLoop(&root, 8, 8);
-// InsertLoop(&root, 20, 20);
-// InsertLoop(&root, 19, 19);
-// InsertLoop(&root, 18, 18);
-// InsertLoop(&root, 17, 17);
-
-// InsertLoop(&root, 40, 40);
-// InsertLoop(&root, 41, 41);
-// InsertLoop(&root, 35, 35);
-// InsertLoop(&root, 30, 30);
-// InsertLoop(&root, 36, 36);
-// InsertLoop(&root, 38, 38);
-// InsertLoop(&root, 37, 37);
-
-// removeNode(root, 20);
-
-
-// searchRecursion(root, 17, ROOT);
-
 
 }
